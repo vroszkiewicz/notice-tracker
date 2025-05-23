33a89a776d106675cb3d6ba09a97fca346972b49
vroszkiewicz <VRoszkiewicz@howey.org>
2025-05-23 11:34:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,1 @@
-import streamlit as st
-import pandas as pd
-from datetime import datetime, timedelta
-import holidays
 
-# US federal holidays 
-us_holidays = holidays.UnitedStates()
-
-# Helper function to calculate 10 calendar days, excluding holidays
-def subtract_days_excluding_holidays(meeting_date, days_required):
-  date = meeting_date
-  valid_days = 0
-  
